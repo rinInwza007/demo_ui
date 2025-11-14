dc71ac5a1f48af5d5680134d8175463bbbe61807
Radit Shalom <radidchalom@gmail.com>
2025-11-14 12:57:46 +0700

    เพิ่มรายการลูกหนี้

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/pages/home.vue'
 import CreateBlade from '@/pages/createblade.vue'
+import Createblades from '@/pages/createblades.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home },
   { path: '/createblade', name: 'createblade', component: CreateBlade },
+  { path: '/createblades', name: 'createblades', component: Createblades },
+
+
 ]
 
 const router = createRouter({

1492cbe02d4837397a20def021e2a0149f079fb1
rinInwza007 <150318717+rinInwza007@users.noreply.github.com>
2025-11-19 09:16:15 +0700

    Update search.vue

--- src/components/input/search.vue
+++ src/components/input/search.vue
@@ -1,22 +1,3 @@
-<template>
-  <form class="relative w-[250px] lg:w-[260px]">
-    <input
-      type="text"
-      placeholder="ค้นหา"
-      class="bg-gray-50 border border-gray-300 h-[44px] w-full block
-             text-black px-[13px] md:px-[16px]
-             placeholder:text-gray-500 outline-0
-             rounded-full"
-    />
-    <button
-      type="button"
-      class="absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 hover:text-gray-700 grid"
-    >
-     <span class="material-symbols-outlined hover:text-blue-700 grid">search</span>
-    </button>
-  </form>
-</template>
-
 <script lang="ts">
 export default {
   name: "SearchForm",

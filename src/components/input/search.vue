bd170a8c02436cefd1c8f50bfaa079bae6b3da89
rinInwza007 <150318717+rinInwza007@users.noreply.github.com>
2025-11-19 09:14:37 +0700

    nowv2

--- src/components/input/search.vue
+++ src/components/input/search.vue
@@ -3,25 +3,22 @@
     <input
       type="text"
       placeholder="ค้นหา"
-      class="bg-gray-50 border border-gray-300 h-[44px] rounded-full w-full block
+      class="bg-gray-50 border border-gray-300 h-[44px] w-full block
              text-black px-[13px] md:px-[16px]
              placeholder:text-gray-500 outline-0
-             rounded-md  "
+             rounded-full"
     />
     <button
       type="button"
-      class="absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 hover:text-gray-700 grid "
+      class="absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 hover:text-gray-700 grid"
     >
      <span class="material-symbols-outlined hover:text-blue-700 grid">search</span>
     </button>
   </form>
 </template>
 
-
 <script lang="ts">
 export default {
-  name: "SearchFrom",
+  name: "SearchForm",
 };
 </script>
-
-

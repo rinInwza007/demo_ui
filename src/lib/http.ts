00855377dd864f8c9c9135979e70ef60038c4dca
Radit Shalom <radidchalom@gmail.com>
2026-02-04 09:36:05 +0700

    Revert "1"
    This reverts commit 7e56b97d1fdfdd2529111c8073834c4542b3a12e.

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,41 +1,12 @@
-// import axios from 'axios'
-
-// const http = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   timeout: 15000,
-// })
-
-// // 🔐 ใส่ token ให้อัตโนมัติ (ถ้ามี)
-// http.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('access_token')
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`
-//     }
-//     return config
-//   },
-//   (error) => Promise.reject(error)
-// )
-
-// // ❌ จัดการ error กลาง
-// http.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     // Network / CORS error
-//     if (!error.response) {
-//       console.error('🌐 Network / CORS error:', error)
-//     }
-//     return Promise.reject(error)
-//   }
-// )
-
-
-
-// export default http
 import axios from 'axios'
 
-// ใส่ token
-axios.interceptors.request.use(
+const http = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 15000,
+})
+
+// 🔐 ใส่ token ให้อัตโนมัติ (ถ้ามี)
+http.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('access_token')
     if (token) {
@@ -46,10 +17,16 @@ axios.interceptors.request.use(
   (error) => Promise.reject(error)
 )
 
-// handle error
-axios.interceptors.response.use(
-  (res) => res,
-  (err) => Promise.reject(err)
+// ❌ จัดการ error กลาง
+http.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Network / CORS error
+    if (!error.response) {
+      console.error('🌐 Network / CORS error:', error)
+    }
+    return Promise.reject(error)
+  }
 )
 
-export default axios
+export default http

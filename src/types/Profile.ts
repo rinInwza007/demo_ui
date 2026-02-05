0e7152e4f149cb4f9412ac4fe0e20ebc4246c395
rinInwza007 <150318717+rinInwza007@users.noreply.github.com>
2026-02-05 09:57:17 +0700

    Update Profile.ts

--- src/types/Profile.ts
+++ src/types/Profile.ts
@@ -1,11 +1,13 @@
 //Profile.ts
-import { roleType  } from "./user"
+import { roleType } from "./user"
+export type sendmoney = 'รายได้' | 'เงินโครงการ'
+
 
 export interface Profile {
   fullName: string
   phone: string
+  role:roleType
   fundName: string
-  role :roleType
   projectCode: string | null
   mainAffiliationId?: string
   mainAffiliationName?: string
@@ -15,6 +17,5 @@ export interface Profile {
   subAffiliationName2?: string
   affiliationId: string
   affiliationName: string
-    moneyType: string
-  sendmoney?: string
+  sendmoney?: sendmoney
 }
